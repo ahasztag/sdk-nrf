4c2826f1c7e1b3d3b69284f00232f8d17b2f66b6
Artur Hadasz <artur.hadasz@nordicsemi.no>
2022-10-06 17:37:26 +0200

    WIP

--- subsys/mpsl/fem/nrf21540_gpio_spi/models/mpsl_fem_nrf21540_power_model_builtin.c
+++ subsys/mpsl/fem/nrf21540_gpio_spi/models/mpsl_fem_nrf21540_power_model_builtin.c
@@ -14,37 +14,48 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 
+
 static int32_t last_temperature = INT32_MIN;
 
-static void model_update_work_handler(struct k_work *work)
+static void model_update_perform(void)
 {
 	mpsl_fem_external_conditions_t external_conditions;
 	int32_t current_temperature = mpsl_temperature_get();
 
 	if (last_temperature != current_temperature) {
-		external_conditions.temperature = mpsl_temperature_get() / 4;
+		external_conditions.temperature = current_temperature / 4;
 		external_conditions.voltage = CONFIG_MPSL_FEM_POWER_VOLTAGE / 100;
 		mpsl_fem_nrf21540_power_model_builtin_update(&external_conditions);
+        last_temperature = current_temperature;
 	}
 }
 
-K_WORK_DEFINE(model_update_work, model_update_work_handler);
-
-static void model_update_timeout(struct k_timer *dummy)
+static void model_update_thread_function(void *unused1, void *unused2, void *unused3)
 {
-	k_work_submit(&model_update_work);
+    (void) unused1;
+    (void) unused2;
+    (void) unused3;
+
+	while (true)
+	{
+        k_sleep(K_MSEC(CONFIG_MPSL_FEM_BUILTIN_POWER_MODEL_UPDATE_PERIOD));
+        model_update_perform();
+	}
 }
 
-K_TIMER_DEFINE(model_update_timer, model_update_timeout, NULL);
+static K_THREAD_STACK_DEFINE(model_thread_stack, 512);
+struct k_thread model_update_thread;
 
 static int model_periodic_update_start(const struct device *unused)
 {
 	/* Perform initial model update */
-	model_update_work_handler(NULL);
+	model_update_perform();
 
-	k_timer_start(&model_update_timer,
-				K_MSEC(CONFIG_MPSL_FEM_BUILTIN_POWER_MODEL_UPDATE_PERIOD),
-				K_MSEC(CONFIG_MPSL_FEM_BUILTIN_POWER_MODEL_UPDATE_PERIOD));
+	k_thread_create(&model_update_thread, model_thread_stack,
+	                K_THREAD_STACK_SIZEOF(model_thread_stack),
+	                model_update_thread_function,
+	                NULL, NULL, NULL,
+	                K_LOWEST_APPLICATION_THREAD_PRIO, 0, K_NO_WAIT);
 
 	return 0;
 }
@@ -55,3 +66,4 @@ const mpsl_fem_power_model_t *mpsl_fem_power_model_to_use_get(void)
 }
 
 SYS_INIT(model_periodic_update_start, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
