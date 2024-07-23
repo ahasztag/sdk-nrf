b923e66e40af93c460322a7ed8a859d32466f2fe
Artur Hadasz <artur.hadasz@nordicsemi.no>
2024-07-23 16:34:44 +0200

    working_key_unwrap

--- samples/crypto/aes_gcm/src/main.c
+++ samples/crypto/aes_gcm/src/main.c
@@ -77,6 +77,19 @@ static uint8_t m_encrypted_text[]
 static uint8_t m_decrypted_text[NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE];
 
 static psa_key_id_t key_id;
+
+static uint8_t m_kek_key[] = {
+0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+};
+
+static uint8_t encrypted_cek[] = {
+0x28, 0xC9, 0xF4, 0x04, 0xC4, 0xB8, 0x10, 0xF4, 0xCB, 0xCC, 0xB3, 0x5C, 0xFB, 0x87, 0xF8, 0x26, 0x3F, 0x57, 0x86, 0xE2, 0xD8, 0x0E, 0xD3, 0x26,
+0xCB, 0xC7, 0xF0, 0xE7, 0x1A, 0x99, 0xF4, 0x3B, 0xFB, 0x98, 0x8B, 0x9B, 0x7A, 0x02, 0xDD, 0x21,
+};
+
+static psa_key_id_t m_kek_key_id;
+
 /* ====================================================================== */
 
 int crypto_init(void)
@@ -105,6 +118,127 @@ int crypto_finish(void)
 
 	return APP_SUCCESS;
 }
+
+psa_status_t arha_key_unwrap(psa_key_id_t kek_key_id, psa_algorithm_t key_enc_alg, uint8_t* wrapped_cek,
+		    size_t cek_bits, uint8_t* unwrapped_cek)
+{
+
+	psa_status_t status = PSA_SUCCESS;
+	/* TODO: verify wrapped_cek is of correct length */
+
+	/* The variable names are matching the names from RFC3394, chapter 2.2.2 (second algorithm variant) */
+	size_t n = cek_bits / 64;
+	uint8_t t = 0; /* Maximum value for t is 24 -> can be uint8_t */
+	uint8_t A[8] = {0}; /* A is a 64 bit value*/
+	uint8_t B[16] = {0};
+	/* 5: 256/64 + 1 - 256 is the maximum key size in bits,the algorithm is working on 64 bits values,
+	   need n + 1 values.
+	   An additional 1 is added to allow indexing R from 1 as in the specification.
+	   */
+	uint8_t R[5+1][8] = {{0}};
+
+	uint8_t temp[16] = {0};
+	// uint8_t temp_buf[16] = {0};
+	size_t temp_output_length = 0;
+	const static uint8_t kw_iv[8] = {0xA6, 0xA6, 0xA6, 0xA6, 0xA6, 0xA6, 0xA6, 0xA6};
+
+	/* Initialize the variables */
+	memcpy(A, wrapped_cek, 8);
+
+	for (size_t i = 1; i <= 5; i++) {
+		wrapped_cek += 8;
+		memcpy(R[i], wrapped_cek, 8);
+	}
+
+	/* Compute intermediate values */
+	for (int j = 5; j >= 0; j--) {
+		for (size_t i = n; i > 0; i--) {
+			t = n * j + i;
+			A[7] = A[7] ^ t; /* Only the LSB will be affected by the xor, as t is always smaller than 256 */
+			memcpy(temp, A, 8);
+			memcpy(temp + 8, R[i], 8);
+			status = psa_cipher_decrypt(kek_key_id,
+                                key_enc_alg,
+                                temp,
+                                sizeof(temp),
+                                B,
+                                sizeof(B),
+                                &temp_output_length);
+
+			if (status != PSA_SUCCESS) {
+				return status;
+			}
+
+			if (temp_output_length != sizeof(B)) {
+				return PSA_ERROR_GENERIC_ERROR;
+			}
+
+			memcpy(A, B, 8);
+			memcpy(R[i], B+8, 8);
+		}
+	}
+
+	if (memcmp(A, kw_iv, 8) != 0){
+		return PSA_ERROR_INVALID_SIGNATURE;
+	}
+
+	for (size_t i = 0; i < n; i++) {
+		memcpy(unwrapped_cek + 8*i, R[i+1], 8);
+	}
+
+	return PSA_SUCCESS;
+}
+
+int import_kek(void)
+{
+	psa_status_t status;
+
+	LOG_INF("importing kek");
+
+	/* Configure the key attributes */
+	psa_key_attributes_t key_attributes = PSA_KEY_ATTRIBUTES_INIT;
+
+	psa_set_key_usage_flags(&key_attributes, PSA_KEY_USAGE_ENCRYPT | PSA_KEY_USAGE_DECRYPT);
+	psa_set_key_lifetime(&key_attributes, PSA_KEY_LIFETIME_VOLATILE);
+	// psa_set_key_algorithm(&key_attributes, PSA_ALG_CCM_STAR_NO_TAG);
+	// psa_set_key_algorithm(&key_attributes, PSA_ALG_CBC_NO_PADDING);
+	psa_set_key_algorithm(&key_attributes, PSA_ALG_ECB_NO_PADDING);
+	// psa_set_key_algorithm(&key_attributes, PSA_ALG_CTR);
+	psa_set_key_type(&key_attributes, PSA_KEY_TYPE_AES);
+	psa_set_key_bits(&key_attributes, 256);
+
+	status = psa_import_key(&key_attributes,
+                            m_kek_key,
+                            sizeof(m_kek_key),
+                            &m_kek_key_id);
+
+	if (status != PSA_SUCCESS) {
+		LOG_INF("psa_import failed! (Error: %d)", status);
+		return APP_ERROR;
+	}
+
+	LOG_INF("KEK imported successfully!");
+
+	return 0;
+}
+
+int unwrap_test(void)
+{
+	import_kek();
+	uint8_t cek[32] = {0};
+	psa_status_t status = PSA_SUCCESS;
+	// status = arha_key_unwrap(m_kek_key_id, PSA_ALG_CBC_NO_PADDING, encrypted_cek, 256, cek);
+	status = arha_key_unwrap(m_kek_key_id, PSA_ALG_ECB_NO_PADDING, encrypted_cek, 256, cek);
+	// status = arha_key_unwrap(m_kek_key_id, PSA_ALG_CTR, encrypted_cek, 256, cek);
+	if (status != PSA_SUCCESS) {
+		LOG_INF("arha_key_unwrap failed! (Error: %d)", status);
+		return APP_ERROR;
+	}
+	LOG_INF("arha_key_unwrap successful!");
+	PRINT_HEX("CEK", cek, sizeof(cek));
+	psa_destroy_key(m_kek_key_id);
+}
+
 
 int generate_key(void)
 {
@@ -232,33 +366,34 @@ int main(void)
 		return APP_ERROR;
 	}
 
-	status = generate_key();
-	if (status != APP_SUCCESS) {
-		LOG_INF(APP_ERROR_MESSAGE);
-		return APP_ERROR;
-	}
-
-	PRINT_HEX("IV", m_iv, sizeof(m_iv));
-	PRINT_HEX("Additional data", m_additional_data, sizeof(m_additional_data));
-	PRINT_HEX("Plaintext", m_plain_text, sizeof(m_plain_text));
-	PRINT_HEX("Encrypted text", m_encrypted_text, sizeof(m_encrypted_text));
-	// status = encrypt_aes_gcm();
+	unwrap_test();
+	// status = generate_key();
 	// if (status != APP_SUCCESS) {
 	// 	LOG_INF(APP_ERROR_MESSAGE);
 	// 	return APP_ERROR;
 	// }
 
-	status = decrypt_aes_gcm();
-	if (status != APP_SUCCESS) {
-		LOG_INF(APP_ERROR_MESSAGE);
-		return APP_ERROR;
-	}
+	// PRINT_HEX("IV", m_iv, sizeof(m_iv));
+	// PRINT_HEX("Additional data", m_additional_data, sizeof(m_additional_data));
+	// PRINT_HEX("Plaintext", m_plain_text, sizeof(m_plain_text));
+	// PRINT_HEX("Encrypted text", m_encrypted_text, sizeof(m_encrypted_text));
+	// // status = encrypt_aes_gcm();
+	// // if (status != APP_SUCCESS) {
+	// // 	LOG_INF(APP_ERROR_MESSAGE);
+	// // 	return APP_ERROR;
+	// // }
 
-	status = crypto_finish();
-	if (status != APP_SUCCESS) {
-		LOG_INF(APP_ERROR_MESSAGE);
-		return APP_ERROR;
-	}
+	// status = decrypt_aes_gcm();
+	// if (status != APP_SUCCESS) {
+	// 	LOG_INF(APP_ERROR_MESSAGE);
+	// 	return APP_ERROR;
+	// }
+
+	// status = crypto_finish();
+	// if (status != APP_SUCCESS) {
+	// 	LOG_INF(APP_ERROR_MESSAGE);
+	// 	return APP_ERROR;
+	// }
 
 	LOG_INF(APP_SUCCESS_MESSAGE);
 
